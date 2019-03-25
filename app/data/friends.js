bc0130d3b568e73bdd81db18e8509fb1a5affa65
Moonkitten <lauragreer3@gmail.com>
2019-03-24 21:46:18 -0600

    add image links

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@
   "all_friends": [
     {
       "name": "Ahmed",
-      "photo": "photour1.jpg",
+      "photo": "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/16/104656161-GettyImages-688156110.1910x1000.jpg",
       "scores[]": [
         5,
         1,
@@ -18,8 +18,8 @@
       "total_score": 34
     },
     {
-      "name": "Bobed",
-      "photo": "photour2.jpg",
+      "name": "Donny",
+      "photo": "https://www.gannett-cdn.com/presto/2019/03/21/USAT/b4c86668-1f41-41a0-8e7a-3c6edb3fe124-EPA_USA_TRUMP_MUELLER.JPG?width=540&height=&fit=bounds&auto=webp",
       "scores[]": [
         3,
         2,
@@ -36,7 +36,7 @@
     },
     {
       "name": "Laura",
-      "photo": "pic",
+      "photo": "http://www.saveacat.org/uploads/4/8/4/1/48413975/1454901839.png",
       "scores[]": [
         "3",
         "2",
